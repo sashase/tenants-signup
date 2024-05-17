d74852ede47c8a8f32be475b373cb0fb2ad52e5f
sashase <semenchuksasha21@gmail.com>
2024-05-17 23:46:03 +0200

    feat: docker setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig, } from "vite"
+import { ConfigEnv, defineConfig, loadEnv, } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),],
-},)
+export default ({ mode, }: ConfigEnv,) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(),), }
+
+  return defineConfig({
+    plugins: [react(),],
+    server: {
+      port: parseInt(process.env.VITE_PORT ?? "8000",),
+    },
+  },)
+}
